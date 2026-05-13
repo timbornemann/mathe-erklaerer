6c8ed2cca24a354a6a516b68eda8c3d82ee4a437
Tim Bornemann <timbornemann@users.noreply.github.com>
2026-05-13 18:31:15 +0200

    Update package-lock.json
    Add autoprefixer and update dependency tree.

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './App.tsx',
+    './components/**/*.{ts,tsx}',
+    './hooks/**/*.{ts,tsx}',
+    './services/**/*.{ts,tsx}',
+    './constants/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
